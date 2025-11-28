c7505228a20ea6db5d13fe84300b8daee7577bef
manfoya <manfoya83@gmail.com>
2025-11-28 19:33:32 +0100

    gestion des tokens JWT

--- app/core/security.py
+++ app/core/security.py
@@ -1,7 +1,16 @@
 # app/core/security.py
 from passlib.context import CryptContext
+from jose import jwt
+from typing import Optional
+from datetime import datetime, timedelta
 
-# On configure le contexte de hachage (Bcrypt est robuste)
+
+# 
+SECRET_KEY = "TEMI_PROJECT_SECRET_KEY_CHANGE_ME"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 # 24 heures
+
+# On configure le contexte de hachage (Bcrypt est bien pour ça)
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
@@ -11,3 +20,15 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 def get_password_hash(password: str) -> str:
     """Transforme un mot de passe en hash illisible"""
     return pwd_context.hash(password)
+
+#
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
