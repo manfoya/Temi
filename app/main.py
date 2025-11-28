0cbfe65cc11f52ab5c5b9f6c03652fe3055d28d3
manfoya <manfoya83@gmail.com>
2025-11-28 19:36:58 +0100

    branchement sur le main

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from app.core.database import engine, Base
 # Il faut importer les modèles pour que SQLAlchemy les détecte lors du create_all
 from app.models import user, academic, pedagogy, grade
-from app.api.v1 import users, students, pedagogy, grades
+from app.api.v1 import users, students, pedagogy, grades, auth
 
 
 # Création des tables dans la base de données
@@ -17,6 +17,7 @@ app.include_router(users.router, prefix="/api/v1", tags=["Utilisateurs"])
 app.include_router(students.router, prefix="/api/v1/students", tags=["Étudiants"])
 app.include_router(pedagogy.router, prefix="/api/v1/academic", tags=["Pédagogie (Admin)"])
 app.include_router(grades.router, prefix="/api/v1/grades", tags=["Notes"])
+app.include_router(auth.router, prefix="/api/v1/auth", tags=["Authentification"])
 
 @app.get("/")
 def read_root():
