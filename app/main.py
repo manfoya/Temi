5e1fabae33a183ed072fd3ae9d836d4677b52ebd
Jahwu Alléluyah Emmanuel DEDEWANOU <manfoya83@gmail.com>
2025-12-01 04:22:08 +0100

    Enregistrement routes academic dans main

--- app/main.py
+++ app/main.py
@@ -1,21 +1,17 @@
 # app/main.py
 from fastapi import FastAPI
 from app.core.database import engine, Base
-# Il faut importer les modèles pour que SQLAlchemy les détecte lors du create_all
 from app.models import user, academic, pedagogy, grade, career
-from app.api.v1 import users, students, pedagogy, grades, auth, career, advisor
+from app.api.v1 import users, students, pedagogy, grades, auth, career, advisor, academic as academic_routes
 
-
-# Création des tables dans la base de données
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Temi API")
 
-# Inclusion des routes
-# prefix="/api/v1" signifie que toutes les urls commenceront par ça
 app.include_router(users.router, prefix="/api/v1", tags=["Utilisateurs"])
 app.include_router(students.router, prefix="/api/v1/students", tags=["Étudiants"])
 app.include_router(pedagogy.router, prefix="/api/v1/academic", tags=["Pédagogie (Admin)"])
+app.include_router(academic_routes.router, prefix="/api/v1/academic", tags=["Années Scolaires"])
 app.include_router(grades.router, prefix="/api/v1/grades", tags=["Notes"])
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["Authentification"])
 app.include_router(career.router, prefix="/api/v1/career", tags=["Carrière & IA"])
@@ -23,6 +19,5 @@ app.include_router(advisor.router, prefix="/api/v1/advisor", tags=["IA & Coachin
 
 @app.get("/")
 def read_root():
-    return {"message": "API Temi en ligne - Tables créées"}
-
+    return {"message": "API Temi en ligne"}
 
