b03ec9bc4554171bff2b6ddd084b0b756d887fd2
Jahwu Alléluyah Emmanuel DEDEWANOU <manfoya83@gmail.com>
2025-12-01 03:36:14 +0100

    Middleware get current ajouté dans security

--- app/core/security.py
+++ app/core/security.py
@@ -1,27 +1,29 @@
 # app/core/security.py
 from passlib.context import CryptContext
-from jose import jwt
+from jose import jwt, JWTError
 from typing import Optional
 from datetime import datetime, timedelta
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from app.core.database import get_db
 
-
-# 
 SECRET_KEY = "TEMI_PROJECT_SECRET_KEY_CHANGE_ME"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 # 24 heures
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
 
-# On configure le contexte de hachage (Bcrypt est bien pour ça)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    """Vérifie si le mot de passe correspond au hash en BDD"""
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password: str) -> str:
     """Transforme un mot de passe en hash illisible"""
+def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
-#
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
@@ -32,3 +34,26 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    from app.models.user import User
+    
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        matricule: str = payload.get("sub")
+        if matricule is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    
+    user = db.query(User).filter(User.matricule == matricule).first()
+    if user is None or not user.is_active:
+        raise credentials_exception
+    
+    return user
